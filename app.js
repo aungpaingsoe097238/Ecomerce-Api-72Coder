8a090af0742e832323fab6f62ba532fe4a98c273
Aung Paing soe <aungpaingsoe097238@gmail.com>
2023-12-28 21:59:04 +0630

    📱 connect with permit router in app.js

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ mongoose.connect(
   `mongodb+srv://admin:adminpass@cluster0.45swmim.mongodb.net/${process.env.DB_NAME}`
 );
 
+const permitRouter = require("./routes/permit");
+
 app.use(express.json());
+
+app.use("/permits", permitRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
